576c65a43cc540af41be94c52cc261541dd6aa67
kirimijesse <rimijessse@gmail.com>
2020-03-13 14:01:47 +0300

    templates

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+path('password-change/', views.PasswordChangeView.as_view(), name='password_change'),
+path('password-change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
