bc89e96b6a3cc7cf7a6877482e1ec2651a0fa541
kirimijesse <rimijessse@gmail.com>
2020-03-15 20:57:32 +0300

    home and signup routes

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,14 @@
-from django.contrib.auth import views
+#from django.contrib.auth import views
 from django.urls import path, include
+from .views import HomePageView, signup
 
 urlpatterns = [
-	path('login/', views.LoginView.as_view(), name='login'),
-	path('logout/', views.LogoutView.as_view(), name='logout')
-	path('password-reset/', views.PasswordResetView.as_view(), name='password_reset'),
-	path('password-reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-	path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-	path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+	path('', HomePageView.as_view(), name='home'),
+	path('signup/', signup, name='signup')
+	#iew.as_view(), name='login'),
+	#path('logout/', views.LogoutView.as_view(), name='logout')
+	#path('password-reset/', views.PasswordResetView.as_view(), name='password_reset'),
+	#path('password-reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+	#path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+	#path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
